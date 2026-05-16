a09f83dd2805c6a11fa11c89d83c63988d324ca2
Vaibhav Gupta <vaibhav0527gupta@gmail.com>
2026-05-17 01:23:41 +0530

    backend setup

--- DevConnect/client/src/pages/Home.jsx
+++ DevConnect/client/src/pages/Home.jsx
@@ -1,6 +1,34 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 function Home() {
+
+  useEffect(() => {
+
+    async function fetchData() {
+
+      try {
+
+        const response = await axios.get(
+          "http://localhost:5000/api/test"
+        );
+
+        console.log(response.data);
+
+      } catch (error) {
+
+        console.log(error);
+
+      }
+
+    }
+
+    fetchData();
+
+  }, []);
+
   return (
-    <div className="text-white text-4xl">
+    <div className="text-4xl p-10">
       Home Page
     </div>
   );
